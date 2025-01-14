2c7ca853618954735247d10c5df4716cd18078cb
Mohidul10587 <mohid10587@gmail.com>
2025-01-14 09:31:46 +0600

    ok

--- components/globlal/Footer.tsx
+++ components/globlal/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import FooterMenu from "./FooterMenu";
+import FooterMenu from "../FooterMenu";
 import { fetchSettings } from "@/app/shared/fetchSettingsData";
 
 // Server component
